d787a91be906239c33596c7443db3aad5408659d
madfoxuser5@gmail.com <LetsCodeFun@2024#:)>
2025-02-06 18:35:58 +0300

    Fix Send Report Via Email

--- move_invoice_line/__manifest__.py
+++ move_invoice_line/__manifest__.py
@@ -29,14 +29,15 @@
     'maintainer': 'Madfox solutions',
     'company': 'Madfox solutions',
     'website': 'https://www.madfox.solutions',
-    'depends': ['base', 'sale', 'fleet', 'account', 'purchase'],
+    'depends': ['base', 'sale', 'fleet', 'account', 'purchase', 'account_asset'],
     'data': [
         'views/account_move_views.xml',
         'views/sale_order_views.xml',
         'views/invoice_views.xml',
         'views/inherit_partner.xml',
         'views/inherit_purchase_order_views.xml',
-        'report/inherit_purchase_template.xml'
+        'report/inherit_purchase_template.xml',
+        'views/inherit_account_asset_views.xml'
 
     ],
     'qweb': [],
