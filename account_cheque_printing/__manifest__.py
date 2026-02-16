e1f860ce9ae78b080763de5ac2d6438a7c128abe
Rahaf Moualla <rahaf.ma@arados-sof.com>
2026-02-16 10:57:30 +0300

    Update version and adjust cheque wizard view

--- account_cheque_printing/__manifest__.py
+++ account_cheque_printing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Cheque Management',
-    'version': '17.0.1.0.0',
+    'version': '17.0.1.0.1',
     'summary': 'Cheque printing and management with wizard',
     'description': """
         Manage cheques, print them multiple times,
