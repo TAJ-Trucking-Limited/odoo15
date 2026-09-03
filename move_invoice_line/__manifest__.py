4f8b9c425582f6f018f811958e84297716f5ba15
hayan-gh <hayan.aljaber@gmail.com>
2026-09-04 00:54:14 +0300

    [FIX] Restore purchase report rendering

--- move_invoice_line/__manifest__.py
+++ move_invoice_line/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Taj Customizations',
-    'version': '19.0.1.1.9',
+    'version': '19.0.1.1.10',
     'summary': 'customizations for accounting add new fields to help compute profit for track ,route, order',
     'category': 'Accounting',
     'author': 'Madfox',
