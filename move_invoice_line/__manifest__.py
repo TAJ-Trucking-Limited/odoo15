3df2650d34bcfae382afb56ff50e085d48c20543
hayan-gh <hayan.aljaber@gmail.com>
2026-09-04 01:16:04 +0300

    [FIX] Restore green purchase report build

--- move_invoice_line/__manifest__.py
+++ move_invoice_line/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Taj Customizations',
-    'version': '19.0.1.1.15',
+    'version': '19.0.1.1.16',
     'summary': 'customizations for accounting add new fields to help compute profit for track ,route, order',
     'category': 'Accounting',
     'author': 'Madfox',
