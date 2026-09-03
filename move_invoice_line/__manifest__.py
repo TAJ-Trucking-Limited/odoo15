4c2364acddadd9a42806b42355a5d351ca0aa25e
hayan-gh <hayan.aljaber@gmail.com>
2026-09-04 00:50:17 +0300

    [FIX] Remove purchase shipping address block

--- move_invoice_line/__manifest__.py
+++ move_invoice_line/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Taj Customizations',
-    'version': '19.0.1.1.8',
+    'version': '19.0.1.1.9',
     'summary': 'customizations for accounting add new fields to help compute profit for track ,route, order',
     'category': 'Accounting',
     'author': 'Madfox',
