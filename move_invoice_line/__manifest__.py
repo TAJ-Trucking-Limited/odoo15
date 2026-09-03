b3f182efa4e39aa1b15dcb67def402426b9dab22
hayan-gh <hayan.aljaber@gmail.com>
2026-09-04 00:58:07 +0300

    [FIX] Restore safe purchase report template

--- move_invoice_line/__manifest__.py
+++ move_invoice_line/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Taj Customizations',
-    'version': '19.0.1.1.11',
+    'version': '19.0.1.1.12',
     'summary': 'customizations for accounting add new fields to help compute profit for track ,route, order',
     'category': 'Accounting',
     'author': 'Madfox',
