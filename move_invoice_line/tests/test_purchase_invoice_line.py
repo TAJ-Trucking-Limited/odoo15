76f6939a529d8674ccb65395c6c63be13fe6fbda
hayan-gh <hayan.aljaber@gmail.com>
2026-09-14 16:03:24 +0300

    [FIX] Tighten PO header spacing with standard vendor address block

--- move_invoice_line/tests/test_purchase_invoice_line.py
+++ move_invoice_line/tests/test_purchase_invoice_line.py
@@ -185,30 +185,21 @@ class TestPurchaseInvoiceLinePropagation(AccountTestInvoicingCommon):
         )
         self.assertEqual(len(hide_options), 1)
         self.assertEqual(hide_options[0].get('t-value'), 'True')
-        emptied_address = root.findall(
-            ".//t[@t-call='web.external_layout']//t[@t-set='address']"
-        )
-        self.assertEqual(len(emptied_address), 1)
         document = lxml_html.fromstring(
             self._render_purchase_html(purchase_line.order_id)
         )
-        header_vendor = document.xpath('//div[@name="taj_header_vendor_address"]')
-        self.assertEqual(len(header_vendor), 1)
-        self.assertIn(self.vendor.name, header_vendor[0].text_content())
         address_rows = document.xpath(
             "//div[contains(concat(' ', normalize-space(@class), ' '), "
             "' address ')]"
         )
-        self.assertFalse(any(
-            self.vendor.name in (row.text_content() or '')
-            for row in address_rows
-        ))
+        self.assertEqual(len(address_rows), 1)
+        self.assertIn(self.vendor.name, address_rows[0].text_content())
 
         paperformat = self.env.ref(
             'move_invoice_line.paperformat_purchase_order'
         )
-        self.assertEqual(paperformat.margin_top, 52)
-        self.assertEqual(paperformat.header_spacing, 52)
+        self.assertEqual(paperformat.margin_top, 30)
+        self.assertEqual(paperformat.header_spacing, 30)
         self.assertEqual(
             self.env.ref('purchase.action_report_purchase_order').paperformat_id,
             paperformat,
